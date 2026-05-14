afe670932bb53614b8332987c25ca500e8d7daa5
qiang11.li <2747122881@qq.com>
2026-05-14 19:09:19 +0800

    feat：css模块化

--- MaxKB_React_Nest/apps/client/src/main.tsx
+++ MaxKB_React_Nest/apps/client/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import styles from "./styles/index.module.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<div className={styles.appRoot}>
+			<App />
+		</div>
 	</StrictMode>,
 );
